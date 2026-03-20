57a13845c287ed0616160adb79e5aa5ae654b2da
ojfernandess <odairjosfernandess@gmail.com>
2026-03-20 20:08:50 -0300

    fix(omnichannel-transcript): use createRequire(__filename) for CommonJS i18n load

--- ee/apps/omnichannel-transcript/src/i18n.ts
+++ ee/apps/omnichannel-transcript/src/i18n.ts
@@ -5,7 +5,7 @@ import languages from '@rocket.chat/i18n/dist/languages';
 import i18next from 'i18next';
 import sprintf from 'i18next-sprintf-postprocessor';
 
-const requireJson = createRequire(import.meta.url);
+const requireJson = createRequire(__filename);
 
 const i18n = i18next.use(sprintf);
 
@@ -21,7 +21,7 @@ void i18n.init({
 			extractTranslationNamespaces(
 				// TODO: commonjs is terrible but we don't have esm build yet
 				// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-dynamic-require
-				require(`@rocket.chat/i18n/dist/resources/${language}.i18n.json`) as unknown as Record<string, string>,
+				requireJson(`@rocket.chat/i18n/dist/resources/${language}.i18n.json`) as unknown as Record<string, string>,
 			),
 		]),
 	),
